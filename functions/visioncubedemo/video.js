c5f5a2958e6787f26ddfcd61195e43325c7dd0eb
Abhinav Chalise <chalisezabhinav@gmail.com>
2024-04-08 03:20:50 +0545

    rename link

---
+++ functions/visioncubedemo/video.js
@@ -0,0 +1,17 @@
+async function onRequest(event) {
+  if (event.request.method === 'GET') {
+    const fileContent = await fetch('/assets/[Demo] Vision-aided Mechanical Design for an Autonomous Rubik\'s Cube Solver.mp4')
+    const headers = new Headers(fileContent.headers)
+
+    headers.set('Content-Disposition', 'attachment; filename="[Demo] Vision-aided Mechanical Design for an Autonomous Rubik\'s Cube Solver.mp4"')
+
+    const modifiedResponse = new Response(fileContent.body, {
+      status: fileContent.status,
+      statusText: fileContent.statusText,
+      headers,
+    })
+    return modifiedResponse
+  }
+  return new Response('Not Found', { status: 404 })
+}
+
