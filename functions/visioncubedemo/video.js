4fba3e497af48df282c278a6f026e96b6b6e84b8
Abhinav Chalise <chalisezabhinav@gmail.com>
2024-04-08 03:25:19 +0545

    rename link

--- functions/visioncubedemo/video.js
+++ functions/visioncubedemo/video.js
@@ -1,17 +1,21 @@
 async function onRequest(event) {
   if (event.request.method === 'GET') {
-    const fileContent = await fetch('/assets/[Demo] Vision-aided Mechanical Design for an Autonomous Rubik\'s Cube Solver.mp4')
-    const headers = new Headers(fileContent.headers)
+    try {
+      const fileContent = await fetch('/assets/[Demo] Vision-aided Mechanical Design for an Autonomous Rubik\'s Cube Solver.mp4')
+      const headers = new Headers(fileContent.headers)
 
-    headers.set('Content-Disposition', 'attachment; filename="[Demo] Vision-aided Mechanical Design for an Autonomous Rubik\'s Cube Solver.mp4"')
+      headers.set('Content-Disposition', 'attachment; filename="[Demo] Vision-aided Mechanical Design for an Autonomous Rubik\'s Cube Solver.mp4"')
 
-    const modifiedResponse = new Response(fileContent.body, {
-      status: fileContent.status,
-      statusText: fileContent.statusText,
-      headers,
-    })
-    return modifiedResponse
+      const modifiedResponse = new Response(fileContent.body, {
+        status: fileContent.status,
+        statusText: fileContent.statusText,
+        headers,
+      })
+      return modifiedResponse
+    }
+    catch (err) {
+      return new Response('Not Found', { status: 404 })
+    }
   }
-  return new Response('Not Found', { status: 404 })
 }
 
